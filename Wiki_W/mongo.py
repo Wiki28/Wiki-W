3c2ef0336c1a844f2d19a09065c475489f399de1
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-02-11 22:43:37 +0800

    Update mongo.py

--- Wiki_W/mongo.py
+++ Wiki_W/mongo.py
@@ -3,20 +3,20 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from GreyCilik import MONGO_DB_URI
-from GreyCilik.confing import get_int_key, get_str_key
+from Wiki_W import MONGO_DB_URI
+from Wiki_W.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "GreyCilik"
+MONGO_DB = "Wiki_W"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["GreyCilik"]
+db = client["Wiki_W"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
