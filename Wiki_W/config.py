f4bde053bd7be9f876cac2f55ad588d550724f26
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-02-11 22:45:21 +0800

    Update config.py

--- Wiki_W/config.py
+++ Wiki_W/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/GreyCilik/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Wiki_W/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
