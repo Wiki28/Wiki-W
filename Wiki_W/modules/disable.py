6f38e5612640dec30f8361fd131e477e8a50ad98
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 22:13:03 +0800

    Update disable.py

--- Wiki_W/modules/disable.py
+++ Wiki_W/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from GreyCilik import dispatcher
-from GreyCilik.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from GreyCilik.modules.helper_funcs.misc import is_module_loaded
+from Wiki_W import dispatcher
+from Wiki_W.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from Wiki_W.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
@@ -14,7 +14,7 @@ from telegram.ext import (
     RegexHandler,
 )
 from telegram.utils.helpers import escape_markdown
-from GreyCilik.modules.language import gs
+from Wiki_W.modules.language import gs
 
 CMD_STARTERS = tuple(CMD_STARTERS)
 
@@ -23,12 +23,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from GreyCilik.modules.helper_funcs.chat_status import (
+    from Wiki_W.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from GreyCilik.modules.sql import disable_sql as sql
+    from Wiki_W.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
