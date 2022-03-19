01fdfcd2b2b7831299f22a38065fd3c86c5bf0f1
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 08:45:32 +0800

    Update reporting_sql.py

--- Wiki_W/modules/sql/reporting_sql.py
+++ Wiki_W/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from GreyCilik.modules.sql import BASE, SESSION
+from Wiki_W.modules.sql import BASE, SESSION
 from sqlalchemy.sql.sqltypes import BigInteger
 from sqlalchemy import Boolean, Column, Integer, String
 
