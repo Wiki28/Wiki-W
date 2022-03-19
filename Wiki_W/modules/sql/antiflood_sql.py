d5e4f9781edef47c76ebb2b6d9477018d7e12822
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 08:32:53 +0800

    Update antiflood_sql.py

--- Wiki_W/modules/sql/antiflood_sql.py
+++ Wiki_W/modules/sql/antiflood_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import String, Column, Integer, UnicodeText
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from GreyCilik.modules.sql import SESSION, BASE
+from Wiki_W.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
