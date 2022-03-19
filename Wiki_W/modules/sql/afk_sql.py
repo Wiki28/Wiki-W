17af2824d0976267b3567ec636eb65c5599d5b0e
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 08:18:42 +0800

    Update afk_sql.py

--- Wiki_W/modules/sql/afk_sql.py
+++ Wiki_W/modules/sql/afk_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from GreyCilik.modules.sql import BASE, SESSION
+from Wiki_W.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, UnicodeText
 from sqlalchemy.sql.sqltypes import BigInteger
 
