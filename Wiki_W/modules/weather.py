fc8454f902ec209bc11a6918e62757bbd4244915
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 13:27:36 +0800

    Update weather.py

--- Wiki_W/modules/weather.py
+++ Wiki_W/modules/weather.py
@@ -8,10 +8,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from GreyCilik import *
+from Wiki_W import *
 
-from GreyCilik import telethn as tbot
-from GreyCilik.events import register
+from Wiki_W import telethn as tbot
+from Wiki_W.events import register
 
 
 async def is_register_admin(chat, user):
