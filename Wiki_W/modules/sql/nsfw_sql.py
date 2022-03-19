e3881f3a7a80ab6bdd69edb62bf7ef28f5f20a68
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 08:46:05 +0800

    Update nsfw_sql.py

--- Wiki_W/modules/sql/nsfw_sql.py
+++ Wiki_W/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from GreyCilik.modules.sql import BASE, SESSION
+from Wiki_W.modules.sql import BASE, SESSION
 
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
