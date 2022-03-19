25d366223c519f883027237b2297bcf30499a8d0
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 22:50:06 +0800

    Update math.py

--- Wiki_W/modules/math.py
+++ Wiki_W/modules/math.py
@@ -1,8 +1,8 @@
 import math
 
 import pynewtonmath as newton
-from GreyCilik import dispatcher
-from GreyCilik.modules.disable import DisableAbleCommandHandler
+from Wiki_W import dispatcher
+from Wiki_W.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
