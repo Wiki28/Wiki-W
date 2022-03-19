1426463941e8bd5efb62f670d478c273e2b3277a
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-20 05:38:46 +0800

    Update sample_config.py

--- Wiki_W/sample_config.py
+++ Wiki_W/sample_config.py
@@ -18,7 +18,7 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 2048936969  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Wiki_W"
+    OWNER_USERNAME = "terserah_wiki"
     SUPPORT_CHAT = "WikiTapiGroup"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001658555612
