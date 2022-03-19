b79ee28110a74e5bef214768f6546ea6e8d09596
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 08:48:15 +0800

    Update locks_sql.py

--- Wiki_W/modules/sql/locks_sql.py
+++ Wiki_W/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from GreyCilik.modules.sql import SESSION, BASE
+from Wiki_W.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
