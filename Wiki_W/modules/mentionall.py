cc27ecf6430e4a455d52f88b6df507ae1afd03e3
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 22:15:08 +0800

    Update mentionall.py

--- Wiki_W/modules/mentionall.py
+++ Wiki_W/modules/mentionall.py
@@ -7,7 +7,7 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.errors import UserNotParticipantError
 
-from GreyCilik import telethn as Client
+from Wiki_W import telethn as Client
 
 spam_chats = []
 
