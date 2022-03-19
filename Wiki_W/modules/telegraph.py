f0884c9f114d07152bed2f4fc7875c99ad2d913c
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 18:13:26 +0800

    Update telegraph.py

--- Wiki_W/modules/telegraph.py
+++ Wiki_W/modules/telegraph.py
@@ -1,5 +1,5 @@
-from GreyCilik.events import register
-from GreyCilik import telethn
+from Wiki_W.events import register
+from Wiki_W import telethn
 
 TMP_DOWNLOAD_DIRECTORY = "./"
 from telethon import events
@@ -8,7 +8,7 @@ from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 
-wibu = "GreyCilik"
+wibu = "Wiki_W"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=wibu)
 auth_url = r["auth_url"]
