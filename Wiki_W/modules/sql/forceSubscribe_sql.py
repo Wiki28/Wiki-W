c5668f445df309c8693e0838615248f740f715c8
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 08:49:29 +0800

    Update forceSubscribe_sql.py

--- Wiki_W/modules/sql/forceSubscribe_sql.py
+++ Wiki_W/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from GreyCilik.modules.sql import BASE, SESSION
+from Wiki_W.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
