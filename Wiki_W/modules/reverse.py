1d3f7dcfa1e3a03afa282f002b0e10feafa7c922
Wiki W <99019030+Wiki28@users.noreply.github.com>
2022-03-19 20:52:34 +0800

    Update reverse.py

--- Wiki_W/modules/reverse.py
+++ Wiki_W/modules/reverse.py
@@ -11,8 +11,8 @@ from telegram import InputMediaPhoto, TelegramError
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
-from GreyCilik import dispatcher
-from GreyCilik.modules.disable import DisableAbleCommandHandler
+from Wiki_W import dispatcher
+from Wiki_W.modules.disable import DisableAbleCommandHandler
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36"
